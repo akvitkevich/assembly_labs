46e03a26a327c29b8e6696531aa6a4aa26538170
kvitkevicha29 <45335315+kvitkevicha29@users.noreply.github.com>
2019-07-01 11:06:02 +0300

    Add files via upload

---
+++ labs2_arrays/Ass2_delete_from_2_7.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+	short A[] = { -127, -2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	const int n = 10;
+	for (int i = 0; i < n; ++i)
+	{
+		cout << A[i] << " ";
+	}
+	cout << endl;
+	_asm {
+		xor eax, eax
+	beg :
+		mov ecx, n
+		sub ecx, eax
+		mov ebx, -2
+	checker :
+		cmp ecx, 0
+		je zeros
+		dec ecx
+		add ebx, 2
+		mov dx, A[ebx]
+		cmp dx, 2
+		jl checker
+		cmp dx, 7
+		jg checker
+		inc eax
+	deleter :
+		mov edx, ebx
+		add edx, 2
+		mov dx, A[edx]
+		mov A[ebx], dx
+		add ebx, 2
+		cmp ecx, 0
+		je beg
+		dec ecx
+		jmp deleter
+	zeros :
+		add ebx, 2
+		mov A[ebx], 0
+		dec eax
+		cmp eax, 0
+		je myend
+		jmp zeros
+	myend :
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
---
+++ labs2_arrays/Ass2_delete_from_interval_a_b.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+	signed char A[] = {-127, -2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int n = 10;
+	for (int i = 0; i < n; ++i)
+	{
+		cout << int(A[i]) << " ";
+	}
+	cout << endl;
+	cout << "Введите границы интервала [a,b]: ";
+	char a, b;
+	int a1, b1;
+	cin >> a1 >> b1;
+	a = a1;
+	b = b1;
+	_asm {
+		xor eax, eax
+	beg:
+		mov ecx, n
+		sub ecx, eax
+		mov ebx, -1
+	checker:
+		cmp ecx, 0
+		je zeros
+		dec ecx
+		add ebx, 1
+		mov dl, A[ebx]
+		cmp dl, 0
+		jg ok
+		neg dl
+	ok:
+		cmp dl, a
+		jl checker
+		cmp dl, b
+		jg checker
+		inc eax
+	deleter:
+		mov edx, ebx
+		add edx, 1
+		mov dl, A[edx]
+		mov A[ebx], dl
+		add ebx, 1
+		cmp ecx, 0
+		je beg
+		dec ecx
+		jmp deleter
+	zeros:
+		add ebx, 1
+		mov A[ebx], 0
+		dec eax
+		cmp eax, 0
+		je myend 
+		jmp zeros
+	myend:
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		cout << int(A[i])<< " ";
+	}
+	cout << endl;
+}
