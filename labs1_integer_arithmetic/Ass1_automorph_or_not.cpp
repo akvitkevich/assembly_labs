cb5fdfd885b6fb82e73fc89ad50d4120ea818e80
kvitkevicha29 <45335315+kvitkevicha29@users.noreply.github.com>
2019-07-01 11:03:11 +0300

    Add files via upload

---
+++ labs1_integer_arithmetic/Ass1_automorph_or_not.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	setlocale(LC_ALL, ".1251");
+	int a;
+	cin >> a;
+	_asm {
+		mov eax, a
+		cmp eax, 0
+		je automorph
+		mov bx, 10
+		mov ecx, 1
+	findPowOfTen: ;получаем число 10^n, где n - кол-во цифр числа
+		cdq
+		idiv bx
+		imul ecx, bx
+		cmp eax, 0
+		jne findPowOfTen
+	checker: ;делим 'a'^2 на 10^'n'. если остаток равен 'a', то число - автоморфное
+		mov eax, a
+		imul eax
+		div ecx
+		mov eax, a
+		cmp eax, edx
+		jne notAutomorph
+	automorph:
+	} 
+	cout << "Число " << a << " является автоморфным!\n";
+	return 0;
+	_asm
+	{
+	notAutomorph:
+	}
+	cout << "Число " << a << " не является автоморфным!\n";
+	return 0;
+}
